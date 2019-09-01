135c7b0d480dbf02c338772662b3eef222246aaa
John Beckhoff <john.beckhoff@outlook.com>
2019-09-01 17:11:10 -0500

    Deep dive with routing

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,14 @@ module RoutingApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+      g.stylesheets     true
+      g.javascripts     true
+      g.scaffold_stylesheet false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  
+  namespace :admin do
+    get 'dashboard/main'
+    get 'dashboard/user'
+    get 'dashboard/blog'
+  end
+
+  get 'about', to: 'pages#about'
+  get 'leadgen/advertising/asdf/lead', to: 'pages#contact', as: 'lead'
+  resources :blogs
+
+  resources :posts
+  # globbing
+  get 'posts/*missing', to: 'posts#missing'
+
+  get 'query/:else/:another_one', to: 'pages#something'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
